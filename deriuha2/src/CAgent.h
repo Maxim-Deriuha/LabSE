8830a6e8fa192a865b7348ad63a562f3c27757e4
Acer <demarkosde@gmail.com>
2019-10-03 14:50:03 +0300

    access rights, getters

--- deriuha2/src/CAgent.h
+++ deriuha2/src/CAgent.h
@@ -14,11 +14,14 @@
 using std::string;
 
 class CAgent {
+private:
+	string nickname;
+	int type_specialty;
 public:
 	int unique_identifier;
 		int nickname_length;
-		string nickname;
-		int type_specialty;
+		int get_type_speciality()const { return type_specialty; };
+		string get_nickname()const { return nickname; };
 		CAgent() :unique_identifier(0), nickname_length(0), nickname(), type_specialty(0) {}
 	    CAgent( int unique_identifier, int nickname_lengh, string nickname, int type_specialty);
 	virtual ~CAgent();
--- deriuha2/src/CSkreen.cpp
+++ deriuha2/src/CSkreen.cpp
@@ -18,8 +18,8 @@ void CSkreen::ScreenAgent(const CAgent& agent) {
 	cout << "agent: " << endl << endl;
 	cout << "unique_identifier = " << agent.unique_identifier << endl;
 	cout << "nickname_length= " << agent.nickname_length << endl;
-	cout << "nickname= " << agent.nickname << endl;
-	cout << "type_specialty= " << agent.type_specialty << endl;
+	cout << "nickname= " << agent.get_nickname() << endl;
+	cout << "type_specialty= " << agent.get_type_speciality() << endl;
 }
 
 CSkreen::~CSkreen()
